0f00592acdda7576741d5fda4e6cf8e5934bcdf8
Austin Melms <memyselfandlie@gmail.com>
2017-07-22 22:11:00 -0400

    Added Metrics

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const client = new Discord.Client({
 	disableEveryone: true
 })
 
-client.login(config.token)
+const metrics = require('datadog-metrics')
+metrics.init({
+	apiKey: config.datadog.APIkey,
+	appKey: config.datadog.APPkey,
+	flushIntervalSeconds: 1,
+	prefix: 'protec.'
+})
 
 client.on('message', async msg => {
-
+	metrics.increment('messages.seen')
 	if (msg.channel.type === 'dm') return
 
 	let command = msg.content.slice(config.prefix.length).toLowerCase().split(' ')[0]
@@ -21,6 +27,7 @@ client.on('message', async msg => {
 		.then(() => msg.guild.member(msg.author).kick()
 		.then(() => msg.channel.send(`**but I also attac**: ${msg.author.tag} kicked`))))
 		.catch(err => msg.channel.send('I could not protec and attac for some reason. Is that person\'s role higher than mine? Do I have kick permissions? Do I have manage message permissions?'))
+		metrics.increment('invites.killed')
 	}
 
 	try {
@@ -37,7 +44,7 @@ client.on('message', async msg => {
 })
 
 client.on('guildCreate', guild => {
-
+	metrics.increment('guild.joined')
 	snekfetch
 		.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
 		.set('Authorization', config.dblist)
@@ -49,14 +56,36 @@ client.on('guildCreate', guild => {
 	guild.defaultChannel.send('**I protec**: I will delete any invites sent here\n\n**But I also attac**: I will kick anyone who sends an invite here.')
 
 })
+client.on('guildDelete', () => {
+	metrics.increment('guild.left')
+})
 
 client.once('ready', () => {
 
 	console.log(`[${new Date()}] ${client.user.username} loaded successfully.`)
 
 	client.user.setGame('He Protec')
+
+	setInterval(collectTechnicalStats, 3000)
+	setInterval(collectBotStats, 30000)
 })
 
 process.on('unhandledRejection', err => {
 	console.error(`${Date()}\n Uncaught Promise Error: \n${err.stack}`)
 })
+
+client.login(config.token)
+
+function collectTechnicalStats() {
+	var memUsage = process.memoryUsage()
+	metrics.gauge('ram.rss', (memUsage.rss / 1048576).toFixed())
+	metrics.gauge('ram.heapUsed', (memUsage.heapUsed / 1048576).toFixed())
+	metrics.gauge('ping', client.ping.toFixed(0))
+
+}
+
+function collectBotStats() {
+	metrics.gauge('totalGuilds', client.guilds.size)
+	metrics.gauge('totalUsers', client.users.size)
+
+}
